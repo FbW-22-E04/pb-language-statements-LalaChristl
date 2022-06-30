c40f2a267d46886f2c2a8707a1a3c0837cfe40dc
LalaChristl <lesleyannchristl@gmail.com>
2022-06-30 13:35:32 +0200

    added solution.js file

---
+++ solution.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+console.log("This is my first js file");
+
+// ERROR MESSAGE:
+// Unexpected string - arrows pointing at 'Hello World'  
+// This means there is an error in the string so I would check and make sure the quotes and/or parenthesis are in the correct place.
